56a37aafd5a547145e6b6463e35188d7d67e13ca
Dat Nguyen <dnguyenk2@gmail.com>
2022-04-23 23:25:08 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function() {
+    console.log("The DOM has loaded");
+    const message = document.getElementById("text");
+    text.innerHTML = "This is really cool!";
+    text.appendChild(text);
+});
+  
+  console.log(
+    "This console.log() fires when index.js loads - before DOMContentLoaded is triggered"
+  );
